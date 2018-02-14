e2bb82665f0b3c48ec3beab6138884be55ec9340
Jecsan Blanco <jblancolicano1@buffs.wtamu.edu>
2018-02-13 22:57:09 -0600

    Wired MainActivity Compute button and compute logic.

--- lab03/ComputePriceV2/app/src/main/java/com/example/jb963962/computepricev2/MainActivity.java
+++ lab03/ComputePriceV2/app/src/main/java/com/example/jb963962/computepricev2/MainActivity.java
@@ -1,11 +1,12 @@
 package com.example.jb963962.computepricev2;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -21,29 +22,38 @@ public class MainActivity extends AppCompatActivity {
 
         price = tax_rate = total = 0.0;
         qty  = 0;
-        price_entry = findViewById(R.id.price_entry_field);
-        quantity_entry = findViewById(R.id.quantity_entry_field);
-        tax_entry= findViewById(R.id.tax_entry_field);
+        price_entry = findViewById(R.id.item_price_entry);
+        quantity_entry = findViewById(R.id.item_qty_entry);
+        tax_entry= findViewById(R.id.item_tax_entry);
 
-        total_display= findViewById(R.id.total_display_field);
+        total_display= findViewById(R.id.item_total_entry);
         total_display.setKeyListener(null);
-
-        price_entry.setText("0.0");
-        tax_entry.setText("0.0");
-        total_display.setText("0");
-        quantity_entry.setText("0");
 
         calculate = findViewById(R.id.compute_button);
         calculate.setOnClickListener(new Button.OnClickListener() {
             public void onClick(View v){
-                Toast.makeText(MainActivity.this, "Works!", Toast.LENGTH_SHORT).show();
-                price = Double.parseDouble(price_entry.getText().toString());
-                qty = Integer.parseInt(quantity_entry.getText().toString());
-                tax_rate = Double.parseDouble(tax_entry.getText().toString());
-
-                total = (price*qty) + (price*qty*tax_rate)/100;
-                total_display.setText(String.format("%.2f",total));
+                calculate();
             }
         });
+    }
+    private void calculate(){
+        try {
+            price = Double.parseDouble(price_entry.getText().toString());
+            qty = Integer.parseInt(quantity_entry.getText().toString());
+            tax_rate = Double.parseDouble(tax_entry.getText().toString());
+        }catch(NumberFormatException e) {
+            invalidInputToast();
+            return;
+        }
+
+        total = (price*qty) + (price*qty*tax_rate)/100;
+        total_display.setText(String.format("$%.2f",total));
+
+    }
+    private void invalidInputToast(){
+        Toast toast = Toast.makeText(this,"Invalid Input!",Toast.LENGTH_SHORT) ;
+        TextView v = toast.getView().findViewById(android.R.id.message);
+        v.setTextColor(Color.RED);
+        toast.show();
     }
 }
