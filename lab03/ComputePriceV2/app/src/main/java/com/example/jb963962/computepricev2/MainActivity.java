c1f6fa2589d225835adee3c3fe2ba8eaaf731608
Jecsan Blanco <jblancolicano1@buffs.wtamu.edu>
2018-02-13 23:40:42 -0600

    Added add_item and show_list visability behavior

--- lab03/ComputePriceV2/app/src/main/java/com/example/jb963962/computepricev2/MainActivity.java
+++ lab03/ComputePriceV2/app/src/main/java/com/example/jb963962/computepricev2/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.jb963962.computepricev2;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -23,34 +25,43 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //This will hold all the items entered.
         items = new ArrayList<>();
 
         price = tax_rate = total = 0.0;
         qty  = 0;
 
-
+        name_entry = findViewById(R.id.item_name_entry);
         price_entry = findViewById(R.id.item_price_entry);
         quantity_entry = findViewById(R.id.item_qty_entry);
         tax_entry= findViewById(R.id.item_tax_entry);
 
         total_display= findViewById(R.id.item_total_entry);
-        total_display.setKeyListener(null);
+
+
 
         compute = findViewById(R.id.compute_button);
+        compute.setEnabled(true);
         compute.setOnClickListener(new Button.OnClickListener() {
-
             public void onClick(View v){
-                calculate();
+               if(tryCalculate()){
+                    add_item.setEnabled(true);
+                    show_list.setEnabled(true);
+                }
             }
         });
+
         show_list = findViewById(R.id.show_list_button);
+        show_list.setEnabled(false);
         show_list.setOnClickListener(new Button.OnClickListener() {
 
             public void onClick(View v){
                 Toast.makeText(MainActivity.this, "Show list clicked!", Toast.LENGTH_SHORT).show();
             }
         });
+
         add_item = findViewById(R.id.add_item_button);
+        add_item.setEnabled(false);
         add_item.setOnClickListener(new Button.OnClickListener() {
 
             public void onClick(View v){
@@ -59,19 +70,21 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
-    private void calculate(){
+    // returns true if the calculation was successful
+    private Boolean tryCalculate(){
         try {
             price = Double.parseDouble(price_entry.getText().toString());
             qty = Integer.parseInt(quantity_entry.getText().toString());
             tax_rate = Double.parseDouble(tax_entry.getText().toString());
         }catch(NumberFormatException e) {
             invalidInputToast();
-            return;
+            return false;
         }
 
         total = (price*qty) + (price*qty*tax_rate)/100;
         total_display.setText(String.format("$%.2f",total));
 
+        return true;
     }
     private void invalidInputToast(){
         Toast toast = Toast.makeText(this,"Invalid Input!",Toast.LENGTH_SHORT) ;
