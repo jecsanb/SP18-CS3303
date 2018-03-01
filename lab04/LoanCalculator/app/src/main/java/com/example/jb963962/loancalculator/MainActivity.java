069e20bff406f5874c78e40c37912ab0884c9ee2
Jecsan Blanco <jblancolicano1@buffs.wtamu.edu>
2018-02-28 21:16:54 -0600

    wrote the monthy payment calc function

--- lab04/LoanCalculator/app/src/main/java/com/example/jb963962/loancalculator/MainActivity.java
+++ lab04/LoanCalculator/app/src/main/java/com/example/jb963962/loancalculator/MainActivity.java
@@ -13,12 +13,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button calculate_button, reset_button, amortization_button;
     private EditText loan_amount_entry, apr_entry, loan_term_entry, loan_payment_entry;
     private double  monthly_payment, apr_rate, months;
+    private TextView display_text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +32,10 @@ public class MainActivity extends AppCompatActivity {
         //todo  wire  editText
 
         //todo wire the button listeners
+
+
+        display_text = findViewById(R.id.display);
+        calculateMonthlyPayment(2000.00,6,5);
     }
 
 
@@ -44,11 +51,11 @@ public class MainActivity extends AppCompatActivity {
 
     }
     //todo write the formula for monthly payments
-    private double calculateMonthlyPayment(double principal , double rate, int months){
-        double payment = 0.0;
+    private double calculateMonthlyPayment(double principal , double apr, int years){
+        int months = years*12;
+        double rate = (apr/100)/12; // why is it under 12?
+        return principal*(rate + ( rate /( Math.pow( 1 + rate,months) -1 ) ) );
 
-
-        return payment;
     }
 
 
