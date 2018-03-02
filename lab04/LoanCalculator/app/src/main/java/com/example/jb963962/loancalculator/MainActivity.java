406e0ed0d58c4f4e160a5af84c54bf2a29c0a6a4
Jecsan Blanco <jblancolicano1@buffs.wtamu.edu>
2018-03-02 09:13:38 -0600

    configured number formating on the editTexts on calculate

--- lab04/LoanCalculator/app/src/main/java/com/example/jb963962/loancalculator/MainActivity.java
+++ lab04/LoanCalculator/app/src/main/java/com/example/jb963962/loancalculator/MainActivity.java
@@ -9,6 +9,7 @@ This program calculates the monthly payment given a loan amount, APR
  */
 package com.example.jb963962.loancalculator;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -33,7 +34,6 @@ public class MainActivity extends AppCompatActivity {
         loan_term_entry = findViewById(R.id.term_entry);
         loan_payment_display = findViewById(R.id.payment_entry);
 
-
         //id's attached to buttons
         calculate_button = findViewById(R.id.calculateButton);
         reset_button = findViewById(R.id.resetButton);
@@ -50,9 +50,11 @@ public class MainActivity extends AppCompatActivity {
                     reset_button.setEnabled(true);
                     amortization_button.setEnabled(true);
                     calculateMonthlyPayment();
+                    formatFields();
                 }
             }
         });
+        //buttons listeners
         reset_button.setOnClickListener(new Button.OnClickListener() {
             public void onClick(View v) {
                 resetAllFields();
@@ -61,6 +63,8 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         amortization_button.setOnClickListener(new Button.OnClickListener() {
+            Intent intent = new Intent(MainActivity.this,TableActivity.class);
+
             public void onClick(View v) {
             }
         });
@@ -99,8 +103,19 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    private  void formatFields(){
+        EditText fields[] = {loan_amount_entry,loan_term_entry,loan_payment_display,apr_entry};
+        Double fieldNumbers[] ={principal,years,monthly_paymet,apr_rate};
+        int i = 0;
+        while( i < fields.length -1) {
+            fields[i].setText(String.format("$%,.2f", fieldNumbers[i]));
+            i++;
+        }
+        fields[i].setText(String.format("%.2f",fieldNumbers[i])+ "%");
+    }
+
     private void resetAllFields() {
-        EditText fields[] = {loan_term_entry, apr_entry, loan_amount_entry, loan_payment_display};
+        EditText fields[] = {loan_amount_entry,loan_term_entry,loan_payment_display,apr_entry};
         for (EditText e : fields) {
             e.setText("");
         }
