e3c88ee25133a7f8704e038821d630470855cf87
Jecsan Blancoi <jblancolicano1@buffs.wtamu.edu>
2018-04-05 22:15:46 -0500

    started lab, images imported

--- lab08/MyStore/app/src/main/java/com/example/jecsan/mystore/StoreActivity.java
+++ lab08/MyStore/app/src/main/java/com/example/jecsan/mystore/StoreActivity.java
@@ -3,7 +3,12 @@ package com.example.jecsan.mystore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.HashMap;
+
 public class StoreActivity extends AppCompatActivity {
+    private String menu_item_names;
+    private double menu_item_prices;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
